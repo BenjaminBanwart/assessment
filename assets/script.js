34897f72e57ea5088a12d1862c1c31ddc0c1886b
Benjamin Banwart <benjibanwart@gmail.com>
2021-11-03 20:03:20 -0500

    added functionality of totaling

--- assets/script.js
+++ assets/script.js
@@ -8,76 +8,77 @@ let sugar = 0   // Sugar Sprinkle
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-
-
+// Gingerbread cookie code
 let gbQuantity = 0
 document.getElementById('add-gb').addEventListener('click', function() {
     gbQuantity++
+    total++
     updateQuantitygb(`${gbQuantity}`)
 })
 document.getElementById('minus-gb').addEventListener('click', function() {
     if(gbQuantity > 0) {
         gbQuantity--
+        total--
     }
     updateQuantitygb(`${gbQuantity}`)
 })
 function updateQuantitygb(displayQuantity) {
     let gbQuantity = document.querySelector('#qty-gb')
     gbQuantity.innerHTML = displayQuantity
+    updateTotal(`${total}`)
 }
 
 
 
-
+// Chocolate chip cookie code
 let ccQuantity = 0
 document.getElementById('add-cc').addEventListener('click', function() {
     ccQuantity++
+    total++
     updateQuantitycc(`${ccQuantity}`)
 })
 document.getElementById('minus-cc').addEventListener('click', function() {
     if(ccQuantity > 0) {
         ccQuantity--
+        total--
     }
     updateQuantitycc(`${ccQuantity}`)
 })
 function updateQuantitycc(displayQuantity) {
     let ccQuantity = document.querySelector('#qty-cc')
     ccQuantity.innerHTML = displayQuantity
+    updateTotal(`${total}`)
 }
 
 
 
 
-
+// Sugar sprinkle cookie code
 let ssQuantity = 0
 document.getElementById('add-sugar').addEventListener('click', function() {
     ssQuantity++
+    total++
     updateQuantityss(`${ssQuantity}`)
 })
 document.getElementById('minus-sugar').addEventListener('click', function() {
     if(ssQuantity > 0) {
         ssQuantity--
+        total--
     }
     updateQuantityss(`${ssQuantity}`)
 })
 function updateQuantityss(displayQuantity) {
     let ssQuantity = document.querySelector('#qty-sugar')
     ssQuantity.innerHTML = displayQuantity
-}
-
-
-let total = 0
-const totalUp = document.querySelector('#qty-total')
-
-function addTotals() {
-    total = updateTotal(`${gbQuantity}`) + updateTotal(`${ccQuantity}`) + updateTotal(`${ssQuantity}`)
-}
-
-document.querySelectorAll('.totals').addEventListener('change', function() {
-    total++
     updateTotal(`${total}`)
-})
+}
 
+
+
+
+
+// Total code
+let total = 0
 function updateTotal(displayQuantity) {
     let total = document.querySelector('#qty-total')
     total.innerHTML = displayQuantity
